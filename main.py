bdb64f1d0cb9b541dd80788da7731e33120c58be
saher <saher.com>
2026-02-22 20:03:17 +0200

    app routers and more

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+from Routes import base
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the Mini RAG API"}
+app.include_router(base.base_router)
